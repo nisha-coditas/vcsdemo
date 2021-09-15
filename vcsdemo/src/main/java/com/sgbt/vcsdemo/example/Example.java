70c3a8dfede51da25cfe93fe911fa8382359c584
Nisha <nisha.singh@coditas.com>
2021-09-15 17:04:25 +0530

    Change 2

--- vcsdemo/src/main/java/com/sgbt/vcsdemo/example/Example.java
+++ vcsdemo/src/main/java/com/sgbt/vcsdemo/example/Example.java
@@ -3,5 +3,6 @@ package com.sgbt.vcsdemo.example;
 public class Example {
     public static void main(String[] args) {
         System.out.println("Good Afternoon!");
+        //changes pulled and pushed successfully
     }
 }
